67b7a741d18970df1b09765518a544743633a0c1
Tadeáš Anděl <tadeas.andel@gmail.com>
2021-08-30 13:16:07 +0200

    cmyk

--- EngineProject/EngineProject.cpp
+++ EngineProject/EngineProject.cpp
@@ -52,9 +52,9 @@ int main()
 	// This array contains positions of vertices (will contain also colors eventually)
 	float vertices[] = {
 		//	[     positions     ] [      colors      ]
-			 0.5f, -0.5f,  0.0f,    1.0f, 0.0f, 0.0f, // top right
-			-0.5f, -0.5f,  0.0f,    0.0f, 1.0f, 0.0f, // bottom right
-			 0.0f,  0.5f,  0.0f,    0.0f, 0.0f, 1.0f  // bottom left
+			 0.5f, -0.5f,  0.0f,    1.0f, 1.0f, 0.0f, // top right
+			-0.5f, -0.5f,  0.0f,    0.0f, 1.0f, 1.0f, // bottom right
+			 0.0f,  0.5f,  0.0f,    1.0f, 0.0f, 1.0f  // bottom left
 			//-0.5f,  0.5f,  0.0f   // top left 
 	};
 
